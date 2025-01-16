4b88278fb9d37f91219385db3d36f2ff0d5fcfa6
ahua1010 <shuaiahua@gmail.com>
2025-01-16 17:32:18 +0800

    Sync on 2025/01/16 週四 17:32:18.71

--- git_push.bat
+++ git_push.bat
@@ -1,13 +1,28 @@
 @echo off
+
+:: 工作目錄
+set WORK_DIR=C:\Users\ahua\桌面\zotero-sync
+
+cd /d "%WORK_DIR%"
+
+:: 程式啟動時執行 git pull
+echo Pulling latest changes from remote repository...
+git pull origin main
+
+
+:: 等待 Zotero 關閉
+echo Waiting for Zotero to exit...
+:CHECK_PROCESS
 tasklist | findstr /I "zotero" >nul
 if %errorlevel% equ 0 (
-    echo Commit failed, please exit Zotero first!
-    pause
-) else (
-    echo Running git pull, add, commit, and push...
-    cd "C:\Users\ahua\桌面\zotero-sync"
-    git pull origin main
-    git add .
-    git commit -m "%date% %time%"
-    git push origin main
+    timeout /t 5 >nul
+    goto CHECK_PROCESS
 )
+
+:: Zotero 關閉後執行 git add、commit 和 push
+echo Zotero closed. Running git add, commit, and push...
+git add .
+git commit -m "Sync on %date% %time%"
+git push origin main
+
+exit
