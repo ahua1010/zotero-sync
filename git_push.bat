7919cca8733bf56d119112a7379d801b555add41
ahua1010 <shuaiahua@gmail.com>
2025-01-16 17:47:00 +0800

    2025/01/16 週四 17:47:00.16

--- git_push.bat
+++ git_push.bat
@@ -1,28 +1,24 @@
 @echo off
+:: 定義Zotero執行檔名稱和Git儲存庫路徑
+set ProgramName=zotero
+set RepoPath=C:\Users\ahua\桌面\zotero-sync
 
-:: 工作目錄
-set WORK_DIR=C:\Users\ahua\桌面\zotero-sync
-
-cd /d "%WORK_DIR%"
-
-:: 程式啟動時執行 git pull
-echo Pulling latest changes from remote repository...
-git pull origin main
-
-
-:: 等待 Zotero 關閉
-echo Waiting for Zotero to exit...
-:CHECK_PROCESS
-tasklist | findstr /I "zotero" >nul
+:: 檢查程式是否開啟
+tasklist | findstr /I "%ProgramName%" >nul
 if %errorlevel% equ 0 (
-    timeout /t 5 >nul
-    goto CHECK_PROCESS
-)
+    echo "%ProgramName% is running. Performing git pull..."
+    cd "%RepoPath%"
+    git pull origin main
+    echo Git pull completed.
+) else (
+    echo "%ProgramName% is not running. Performing git push..."
+    cd "%RepoPath%"
+    git add .
+    git commit -m "%date% %time%"
+    git push origin main
+    echo Git push completed.
+    
 
-:: Zotero 關閉後執行 git add、commit 和 push
-echo Zotero closed. Running git add, commit, and push...
-git add .
-git commit -m "Sync on %date% %time%"
-git push origin main
-
+)	
+pause
 exit
